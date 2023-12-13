53fbed0ee0a63382b6f5a10150f1002e179b0388
wu <wu@wu-Lenovo-G5000-APH8>
2023-12-13 21:51:35 +0800

    add normalcookie

--- src/main/java/com/heispeg/ohitoyo/item/ModItem.java
+++ src/main/java/com/heispeg/ohitoyo/item/ModItem.java
@@ -1,5 +1,22 @@
 package com.heispeg.ohitoyo.item;
 
+import com.heispeg.ohitoyo.Ohitoyo;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class ModItem {
+	public static final DeferredRegister<Item> ITEMS =
+			DeferredRegister.create(ForgeRegistries.ITEMS, Ohitoyo.MODID);
+	
+	public static final RegistryObject<Item> NORMAL_COOKIE = ITEMS.register("normalcookie", 
+			() -> new Item(new Item.Properties()));
+	
+	public static void register(IEventBus eventBus) {
+		ITEMS.register(eventBus);
+	}
 
 }
